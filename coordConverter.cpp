10121e2e465cc7ee181f1cc45641f288e6b06b5d
marouault <martin.rouault@etu.uca.fr>
2022-10-23 17:18:23 +0200

    Image chien cropped

--- coordConverter.cpp
+++ coordConverter.cpp
@@ -3,8 +3,10 @@
 std::optional<SDL_Rect> worldToCam(const SDL_Rect& worldCoord, const SDL_Rect& camRectInWorld, const SDL_Rect& camRectInWindow) {
 	int w = (worldCoord.w*camRectInWindow.w)/camRectInWorld.w;
 	int h = (worldCoord.h*camRectInWindow.h)/camRectInWorld.h;
-	return SDL_Rect{(((worldCoord.x-camRectInWorld.x-worldCoord.w/2)*camRectInWindow.w)/camRectInWorld.w),
-			(((worldCoord.y-camRectInWorld.y-worldCoord.h/2)*camRectInWindow.h)/camRectInWorld.h),
+	float x = ((((float)worldCoord.x-(float)camRectInWorld.x-worldCoord.w/2.0)*camRectInWindow.w)/camRectInWorld.w);
+
+	return SDL_Rect{(int)x,
+			(((worldCoord.y-camRectInWorld.y-worldCoord.h/2.0)*camRectInWindow.h)/camRectInWorld.h),
 			w,
 			h
 			};
